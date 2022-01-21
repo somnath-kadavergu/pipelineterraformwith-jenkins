b9c7bc0da2dc87c097239313451d730a7efff583
somnath-kadavergu <61821597+somnath-kadavergu@users.noreply.github.com>
2022-01-21 15:08:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "access_key" {
-  default = "AKIAVFBR3CHWJZY2S3UV"
+  default = "AKIA5ZTJ2QICSB6B434Z"
   }
 variable "secret_key" {
-  default = "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"
+  default = "wXnvf/HJPn+flxmuK9kiP+k2rZL5hg719FWMV8mP"
   }
 variable "region" {
-  default = "ap-south-1"
+  default = "us-east-2"
   }
